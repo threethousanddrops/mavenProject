64b6838138eaf9e53626d0db4f6c981b1ceb9482
threethousanddrops <62058640+threethousanddrops@users.noreply.github.com>
2020-10-26 12:21:31 +0800

    Update WordCount.java

--- src/main/java/com/shakespeare/WordCount.java
+++ src/main/java/com/shakespeare/WordCount.java
@@ -21,31 +21,27 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class WordCount {
 
-  public static class TokenizerMapper 
-         extends Mapper<Object, Text, Text, Text> {
-    //static enum CountersEnum { INPUT_WORDS }
-
+public static class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
+    private static final IntWritable one = new IntWritable(1);
     private Text word = new Text();
-    
-    private Set<String> patternsToSkip=new HashSet<>();
+    private Set<String> patternsToskip=new HashSet<>();
     private Set<String> punctuation=new HashSet<>();
-
     protected void setup(Context context) throws IOException, InterruptedException{
-        FileSystem fstm = FileSystem.get(context.getConfiguration());
+        FileSystem fs = FileSystem.get(context.getConfiguration());
 
         Path path1 = new Path("hdfs://lyyq181850099-master:9000/wordcount/stop-word-list.txt");
-        BufferedReader reader1 = new BufferedReader(new InputStreamReader(fstm.open(path1)));
-        String line;
-        while ((line = reader1.readLine()) != null) {
-            patternsToSkip.add(line.toLowerCase());
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(fs.open(path1)));
+        String liner;
+        while ((liner = reader1.readLine()) != null) {
+          patternsToskip.add(liner.toLowerCase());
         }
         reader1.close();
 
         Path path2 = new Path("hdfs://lyyq181850099-master:9000/wordcount/punctuation.txt");
-        BufferedReader reader2 = new BufferedReader(new InputStreamReader(fstm.open(path2)));
-        String line2;
-        while ((line2 = reader2.readLine()) != null) {
-            punctuation.add(line2.toLowerCase());
+        BufferedReader reader2 = new BufferedReader(new InputStreamReader(fs.open(path2)));
+        String lined;
+        while ((lined = reader2.readLine()) != null) {
+          punctuation.add(lined.toLowerCase());
         }
         reader2.close();
     }
@@ -53,89 +49,85 @@ public class WordCount {
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
         StringTokenizer itr = new StringTokenizer(value.toString());
         while (itr.hasMoreTokens()) {
-            String wordstr=itr.nextToken().toLowerCase();
-            /*for (String pun: punctuation ){
-                wordstr=wordstr.replaceAll(pun,"");
+            String str=itr.nextToken().toLowerCase();
+            str=str.replaceAll("\\d+"," ");
+            for (String pun: this.punctuation ){
+                str=str.replaceAll(pun,"");
             }
-            for (String pskip: patternsToSkip){
-              wordstr=wordstr.replaceAll(pskip,"");
-            }*/
-            wordstr=wordstr.replaceAll("\\d+","");
-            if(this.patternsToSkip.contains(wordstr)||this.punctuation.contains(wordstr)||wordstr.length()<3)
+            if(this.patternsToskip.contains(str)||str.length()<3)
                 continue;
            else{
-                word.set(wordstr);
-                context.write(word, new Text("1"));
-                /*Counter counter = context.getCounter(CountersEnum.class.getName(),
-                CountersEnum.INPUT_WORDS.toString());
-                counter.increment(1);*/
+                this.word.set(str);
+                context.write(this.word, new Text("1"));
             }
+        }
+
+    }
+}
+public static class IntSumReducer extends Reducer<Text, Text, Text, Text> {
+   
+  private IntWritable result = new IntWritable();
+    private Text word = new Text();
+
+    private static TreeMap<Integer, String> treeMap = new TreeMap<Integer, String>(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer x, Integer y) {
+            return y.compareTo(x);
+        }
+    });
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
+    {
+        int sum = 0;
+        for (Text val: values)
+        {
+            sum+=Integer.valueOf(val.toString());
+        }
+        //result.set(sum);
+        treeMap.put(new Integer(sum), key.toString());
+         if(treeMap.size()>100) {
+             treeMap.remove(treeMap.lastKey());
+         }
+        //treeMap.put(sum,key.toString());
+        //context.write(key, result);
+    }
+    protected void cleanup(Context context)
+        throws IOException,InterruptedException{
+        Set<Map.Entry<Integer, String>> set = treeMap.entrySet();
+        int i =1;
+        for (Map.Entry<Integer, String> entry : set) {
+            this.result.set(entry.getKey());
+            this.word.set(i+":"+entry.getValue()+", "+entry.getKey());
+            context.write(word, new Text(""));
+            i++;
         }
     }
   }
-  
-public static class IntSumReducer
-    extends Reducer<Text,Text,Text,Text> {
-  private IntWritable result = new IntWritable();
-  private Text word = new Text();
 
-  private TreeMap<Integer,String> treeMap = new TreeMap<Integer,String>(new Comparator<Integer>(){
-    @Override
-    public int compare(Integer x,Integer y){return y.compareTo(x);}
-  });
-  
-  public void reduce(Text key, Iterable<Text> values,Context context) 
-                    throws IOException, InterruptedException {
-      int sum = 0;
-      for (Text val : values) {
-          sum+= Integer.valueOf(val.toString());
-      }
-      //result.set(sum);
-      treeMap.put(new Integer(sum), key.toString());
-      //treeMap.put(sum,key.toString());
-      //context.write(key, result);
-    }
-
-  protected void cleanup(Context context)
-      throws IOException,InterruptedException{
-    Set<Map.Entry<Integer, String>> set = treeMap.entrySet();
-    int count =1;
-    for (Map.Entry<Integer, String> entry : set) {
-          this.result.set(entry.getKey());
-          this.word.set(count+": "+entry.getValue()+", "+entry.getKey());
-          context.write(word, new Text(""));
-          count++;
-          if(count>100) {return;}
-       }
-     }
-  }
-  
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
 
-    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-    if (otherArgs.length != 3) {
-      System.err.println("Usage: wordcount <in> <out> -skip");
+    GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
+    String[] remainingArgs = optionParser.getRemainingArgs();
+    if (remainingArgs.length !=3) {
+      System.err.println("Usage: wordcount <in> <out> [-skip]");
       System.exit(2);
-    }
-    
+    } 
+
     Job job = Job.getInstance(conf, "word count");
     job.setJarByClass(WordCount.class);
     job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
     job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(Text.class);
 
-    Path fileoutPath=new Path(args[1]);
-        FileSystem f = FileSystem.get(conf);
-        if (f.exists(fileoutPath)) 
-        {
-            f.delete(fileoutPath, true);
-        }
+    Path outPath=new Path(args[1]);
+    FileSystem fstm = FileSystem.get(conf);
+    if (fstm.exists(outPath)) {
+        fstm.delete(outPath, true);
+    }
 
     FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, fileoutPath);
+    FileOutputFormat.setOutputPath(job, outPath);
     System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
+}
 }
